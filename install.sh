c71722949ac93570fee35d3cc997904e885ae0c4
Asif Zubayer Palak <36653429+Acesif@users.noreply.github.com>
2024-02-02 22:01:35 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-pacman -Sy git libxft libxinerama vim neovim wmname udiskie xbindkeys xcompmgr numlockx xwallpaper xdotool
-
+sudo pacman -Sy git libxft libxinerama vim neovim wmname udiskie xbindkeys xcompmgr numlockx xwallpaper xdotool
 sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
 paru -Sy clipmenu-git ttf-ms-fonts
 cd ~
